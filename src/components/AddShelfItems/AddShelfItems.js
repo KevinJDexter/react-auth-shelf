1b96f0316557b01e9f92c9a65a17826203b147c7
Pa Yeng Thao <payengt24@gmail.com>
2018-05-31 15:20:21 -0500

    added post route on client side

--- src/components/AddShelfItems/AddShelfItems.js
+++ src/components/AddShelfItems/AddShelfItems.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../../redux/actions/userActions';
+import Nav from '../Nav/Nav';
+
+
 
 const mapStateToProps = state => ({
     user: state.user,
-  });
+});
 
 class AddShelfItems extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: '',
+            image_url: '',
+        }
+    }
 
     componentDidMount() {
         this.props.dispatch(fetchUser());
@@ -18,12 +29,30 @@ class AddShelfItems extends Component {
         }
     }
 
+    handleChangeFor = (property) => (event) => {
+        this.setState  ({
+            [property]: event.target.value
+        })
+        
+    }
+
+    submitItem = (event) => {
+        event.preventDefault();
+        this.props.dispatch({ type: 'ADD_SHELF_ITEM', payload: this.state})
+        this.setState ({
+            description: '',
+            image_url: '',
+        })
+
+    }
+
     render() {
         return (
             <div>
-                <form>
-                    <input placeholder="Description" />
-                    <input placeholder="Image_url" />
+                <Nav />
+                <form onSubmit={this.submitItem}>
+                    <input placeholder="Description" value={this.state.description} onChange={this.handleChangeFor('description')} />
+                    <input placeholder="Image_url" value={this.state.image_url} onChange={this.handleChangeFor('image_url')} />
                     <input type="submit" />
                 </form>
 
